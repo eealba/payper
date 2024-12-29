491e961fbd9acd4e6de57fcb31c27d707926a636
Edgar Enrique Alba Barrile <eealba@gmail.com>
2024-12-29 12:09:57 +0100

    Define Payments Api

---
+++ payments-v2/src/main/java/io/github/eealba/payper/payments/v2/api/PaymentsApiClient.java
@@ -0,0 +1,72 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.github.eealba.payper.payments.v2.api;
+
+import io.github.eealba.payper.core.PayperConfig;
+
+/**
+ * Abstract class for payments API client.
+ *
+ * @since 1.0
+ * @version 1.0
+ * @author Edgar Alba
+ */
+public abstract class PaymentsApiClient {
+
+    /**
+     * Creates a new PaymentsApiClient instance using the default configuration.
+     * <p>
+     * This method initializes the client with the default settings and credentials
+     * required for communicating with the PayPal Payments API.
+     *
+     * @return a new PaymentsApiClient instance
+     */
+    public static PaymentsApiClient create() {
+        return create(PayperConfig.builder().build());
+    }
+
+    /**
+     * Creates a new PaymentsApiClient instance using the specified configuration.
+     * <p>
+     * This method allows for custom configuration of the client, including setting
+     * specific credentials and other settings required for authenticating and
+     * communicating with the PayPal Payments API.
+     *
+     * @param config the configuration to be used
+     * @return a new PaymentsApiClient instance
+     */
+    public static PaymentsApiClient create(PayperConfig config) {
+        return PaymentsProvider.provider().create(config);
+    }
+    /**
+     * Returns the Authorizations API interface.
+     *
+     * @return the Authorizations API interface
+     */
+    public abstract Authorizations authorizations();
+
+    /**
+     * Returns the Captures API interface.
+     *
+     * @return the Captures API interface
+     */
+    public abstract Captures captures();
+
+    /**
+     * Returns the Refunds API interface.
+     *
+     * @return the Refunds API interface
+     */
+    public abstract Refunds refunds();
+}
---
+++ payments-v2/src/main/java/io/github/eealba/payper/payments/v2/api/PaymentsProvider.java
@@ -0,0 +1,52 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.github.eealba.payper.payments.v2.api;
+
+import io.github.eealba.payper.core.PayperConfig;
+import io.github.eealba.payper.core.util.Providers;
+
+/**
+ * Abstract class for payments provider.
+ *
+ * @since 1.0
+ * @version 1.0
+ * @author Edgar Alba
+ */
+public abstract class PaymentsProvider {
+    private static final String DEFAULT = "io.github.eealba.payper.payments.v2.internal.PaymentsProviderImpl";
+
+    /**
+     * Constructs a new PaymentsProvider.
+     */
+    public PaymentsProvider() {
+
+    }
+
+    /**
+     * Returns the default PaymentsProvider instance.
+     *
+     * @return the default PaymentsProvider instance
+     */
+    public static PaymentsProvider provider() {
+        return Providers.getProvider(PaymentsProvider.class, DEFAULT);
+    }
+
+    /**
+     * Creates a new PaymentsApiClient instance using the specified configuration.
+     *
+     * @param config the configuration to be used
+     * @return a new PaymentsApiClient instance
+     */
+    public abstract PaymentsApiClient create(PayperConfig config);
+}
