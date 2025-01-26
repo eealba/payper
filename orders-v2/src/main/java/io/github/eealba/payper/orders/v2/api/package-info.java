1ab330f0c61b1513de12116c3bc8a970d4eb1894
Edgar Enrique Alba Barrile <eealba@gmail.com>
2025-01-26 13:55:17 +0100

    Examples and documentation

---
+++ orders-v2/src/main/java/io/github/eealba/payper/orders/v2/api/package-info.java
@@ -0,0 +1,15 @@
+/**
+ * Provides classes and interfaces for interacting with the Orders API in the Payper library.
+ * <p>
+ * This package includes the API client and model classes for creating, retrieving, and managing orders.
+ * </p>
+ * <ul>
+ *   <li>{@link io.github.eealba.payper.orders.v2.api.Orders} - Interface for interacting with the Orders API.</li>
+ *   <li>{@link io.github.eealba.payper.orders.v2.api.CheckoutOrdersApiClient} - Client for accessing the Orders API.</li>
+ * </ul>
+ *
+ * @since 1.0
+ * @version 1.0
+ * @author Edgar Alba
+ */
+package io.github.eealba.payper.orders.v2.api;
